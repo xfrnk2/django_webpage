3f047bc79a8a8c655be2ea894081047c4fb23b14
DESKTOP-K28GGME\rad87_home <xfrnk2@gmail.com>
2020-03-20 22:59:21 +0900

    유형 테스트를 위한 기본 틀 구성. name 입력값과 answer 값이 결과페이지에 나타나지 않는 에러가 남아있음.

--- student_management/students/urls.py
+++ student_management/students/urls.py
@@ -17,7 +17,7 @@ from django.urls import path
 from . import views
 app_name = 'students'
 urlpatterns = [
-
+    path('reg/', views.regStudent, name='reg'),
     path('regCon/', views.regConStudent, name='regCon'),
     path('all/', views.readStudentAll, name='stuAll'),
     path('<str:name>/det/', views.detStudent, name='stuDet'),
