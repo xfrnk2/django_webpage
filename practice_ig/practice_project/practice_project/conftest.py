a17d4cfd7900830013884e5186ac57b905bde418
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-04-16 13:06:57 +0900

    instagram practice

---
+++ practice_ig/practice_project/practice_project/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from practice_project.users.models import User
+from practice_project.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
