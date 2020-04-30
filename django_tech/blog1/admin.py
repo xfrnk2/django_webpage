49d3e9cddbde98309d513eed6f87d8db9cd4b16b
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-05-01 00:04:01 +0900

    Revert "modifed TIL"
    This reverts commit 3f01eb9247c451c82b4ec2477a15c5c36da5ca41.

--- django_tech/blog1/admin.py
+++ django_tech/blog1/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from .models import Post, Comment
+from .models import Post
 # Register your models here.
 # admin.site.register(Post)
 
@@ -22,7 +22,3 @@ class PostAdmin(admin.ModelAdmin):
       if post.photo:
             return mark_safe(f'<img src="{post.photo.url}" style="width : 72px;" />')
       return None
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-   pass
