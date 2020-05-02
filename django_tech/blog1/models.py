1c876adfbff908137b6b8cf769bb900aec74862b
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-05-03 01:50:33 +0900

    added author object to Post

--- django_tech/blog1/models.py
+++ django_tech/blog1/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
+from django.conf import settings
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True) #db 저장시간
     updated_at = models.DateTimeField(auto_now=True) # 수정시간
