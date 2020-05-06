8d85652b4a72239a7bcae3361afaaa467f41f718
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-05-06 12:34:04 +0900

    Django Queryset을 통한 간단한 검색 구현

--- django_tech/blog1/urls.py
+++ django_tech/blog1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.add_post, name='post'),
+    # path('', views.add_post, name='post'),
+    path('', views.post_list),
 ]
--- django_tech/blog1/views.py
+++ django_tech/blog1/views.py
@@ -3,3 +3,13 @@ from .models import Post
 def add_post(request):
     qs = Post.objects.all()
     return render(request, 'blog1/post.html', { 'post_list' : qs })
+
+def post_list(request):
+    qs = Post.objects.all()
+    q = request.GET.get('q', '') #없으면 ''를 반환 (검색어가 q에 저장)
+    if q:
+        qs = qs.filter(content__icontains=q)
+    return render(request, 'blog1/post_list.html',
+                  {'post_list' : qs,
+                   'q' : q,
+                  })
