a0ccda4a848af6fabf0ec4004c27ebc7c8a66f25
DESKTOP-GTKC6HS\rad87 <xfrnk2@gmail.com>
2020-05-07 10:58:33 +0900

    add model classes(Post, Comment, Tag)

---
+++ site_for_practice/board/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.conf import settings
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        ordering = ['id']
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return f"{self.name}"
+
+
+
