d9a8c09adbf17b08660d16a23421a1250472248e
xfrnk2 <xfrnk2@gmail.com>
2020-08-10 00:55:00 +0900

    add function in models.py\User : send_welcome_email

--- instagram/accounts/models.py
+++ instagram/accounts/models.py
@@ -1,7 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 class User(AbstractUser):
     website_url = models.URLField(blank=True)
     bio = models.TextField(blank=True)
+
+    def send_welcome_email(self):
+        subject = render_to_string("accounts/welcome_email_subject.txt", {'user':self,})
+        content = render_to_string("accounts/welcome_email_content.txt", {'user':self,})
+        sender_email = settings.WELCOME_EMAIL_SENDER
+        
+        send_mail(subject, content, sender_email, [self.email], fail_silently=False)
+        #send_mail("Subject here", "Here is the message.", "xfrnk2@gmail.com", ["macos1839@gmail.com"], fail_silently=False)
