db0945c31475dcab214c63ff5f9685d04db3fe11
xfrnk2 <xfrnk2@gmail.com>
2020-08-10 01:25:08 +0900

    test

--- instagram/accounts/models.py
+++ instagram/accounts/models.py
@@ -4,7 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     website_url = models.URLField(blank=True)
+<<<<<<< HEAD
     bio = models.TextField(blank=True)
 
     def send_welcome_email(self):
         pass
+=======
+    bio = models.TextField(blank=True)
+>>>>>>> parent of d9a8c09... add function in models.py\User : send_welcome_email
