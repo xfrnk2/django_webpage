346b6d2768fe79ca7a96eeaf4f049df379f14f47
xfrnk2 <xfrnk2@gmail.com>
2020-08-10 01:07:42 +0900

    Revert "add function in models.py\User : send_welcome_email"
    This reverts commit d9a8c09adbf17b08660d16a23421a1250472248e.

--- instagram/accounts/views.py
+++ instagram/accounts/views.py
@@ -1,13 +1,13 @@
 from django.contrib import messages
 from django.shortcuts import redirect, render
 from .forms import SignupForm
+
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            signed_user = form.save()
+            form.save()
             messages.success(request, "회원가입 환영합니다.")
-            signed_user.send_welcome_email() # FIXME : Celery로 처리하는것을 추천.
             next_url = request.GET.get('next', '/')
             return redirect(next_url)
     else:
