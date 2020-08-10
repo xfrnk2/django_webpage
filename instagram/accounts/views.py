a3ccddbdc92800a202db0e14fc40d5f122c4719d
xfrnk2 <xfrnk2@gmail.com>
2020-08-11 00:52:38 +0900

    add : logout function

--- instagram/accounts/views.py
+++ instagram/accounts/views.py
@@ -1,15 +1,22 @@
 from django.contrib import messages
 from django.shortcuts import redirect, render
 from .forms import SignupForm
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, logout_then_login
+from django.contrib.auth import login as auth_login
 
 login = LoginView.as_view(template_name="accounts/login_form.html")
+
+def logout(request):
+    #메세지를 추가하고 싶다면
+    messages.success(request, "로그아웃 되었습니다.")
+    return logout_then_login(request)
 
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
             signed_user = form.save()
+            auth_login(request, signed_user)
             messages.success(request, "회원가입 환영합니다.")
             next_url = request.GET.get('next', '/')
             signed_user.send_welcome_email() #FIXME : Celery로 처리하는것을 추천
