0b21e958d266c5d447e7509328a60be3a2494fc6
xfrnk2 <xfrnk2@gmail.com>
2020-08-11 00:31:51 +0900

    add : return value in account\forms.py

--- instagram/accounts/forms.py
+++ instagram/accounts/forms.py
@@ -5,7 +5,7 @@ from .models import User
 class SignupForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-      
+
         self.fields['email'].required = True
         self.fields['first_name'].required = True
         self.fields['last_name'].required = True
@@ -22,3 +22,4 @@ class SignupForm(UserCreationForm):
             qs = User.objects.filter(email = email)
             if qs.exists():
                 raise forms.ValidationError("이미 등록된 이메일 주소입니다. ")
+        return email
