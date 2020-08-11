294990dfc4bde123c86ec0e0a473c59373a8b299
xfrnk2 <xfrnk2@gmail.com>
2020-08-11 23:20:12 +0900

    add : model options : gender, phonenumber

--- instagram/accounts/models.py
+++ instagram/accounts/models.py
@@ -3,9 +3,21 @@ from django.db import models
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
+from django.core.validators import RegexValidator
+from django.shortcuts import resolve_url
+
 class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = "M", "남성"
+        FEMALE = "F", "여성"
+
+
     website_url = models.URLField(blank=True)
     bio = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=13, blank=True,
+                                    validators=[RegexValidator(r"^010-?[1-9]\d{3}-?\d{4}$")])
+    gender = models.CharField(max_length=1, blank=True, choices=GenderChoices.choices)
+
 
     def send_welcome_email(self):
         subject = render_to_string("accounts/welcome_email_subject.txt", {'user':self})
