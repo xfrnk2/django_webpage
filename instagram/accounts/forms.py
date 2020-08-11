3c6b63425b0819de05636c86739f2a8111470e19
xfrnk2 <xfrnk2@gmail.com>
2020-08-11 23:32:20 +0900

    add : fields in form.py

--- instagram/accounts/forms.py
+++ instagram/accounts/forms.py
@@ -23,7 +23,8 @@ class SignupForm(UserCreationForm):
             if qs.exists():
                 raise forms.ValidationError("이미 등록된 이메일 주소입니다. ")
         return email
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ["last_name", "first_name", "website_url", "bio"]
+        fields = ["last_name", "first_name", "website_url", "bio", "phone_number", "gender"]
