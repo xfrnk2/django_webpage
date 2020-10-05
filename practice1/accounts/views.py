e584d9e68dc78a318b2fee6892c1c075cda14979
xfrnk2 <xfrnk2@gmail.com>
2020-10-05 11:20:38 +0900

    signup 기능 추가

--- practice1/accounts/views.py
+++ practice1/accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.contrib.auth import login as auth_login
+from .forms import SignupForm
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            signed_user = form.save()
+            auth_login(request, signed_user)
+            messages.success(request, "회원가입 환영합니다.")
+            # next_url = request.GET.get('next', '/')
+            # return redirect('admin/')
+            return
+    else:
+        form = SignupForm()
+    return render(request, 'accounts/signup_form.html', {
+        'form' : form,
+    })
